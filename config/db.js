3e829e4ea47b1c6af5c39f97cd6cb70520cfd554
Alish Palasara <palasaraalis@gmail.com>
2021-05-23 23:14:49 +0530

    Basic express setup

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const mongoUri = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database connected ...`);
+  } catch (error) {
+    console.log('Database connection failed !!!');
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ success: true });
-});
+// Loading Route files
+const user = require('./routes/api/user');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
+
+// Connecting to database
+connectDB();
+
+app.use(express.json());
+
+// routes
+app.use('/api/v1/users', user);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/profile', profile);
+app.use('/api/v1/posts', post);
 
 // Running app on port
 const port = process.env.PORT || 5000;
